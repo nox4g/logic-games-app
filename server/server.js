92b61242606550e8bca1a70ebe5b901996c94561
nox4g <bogdan8balyas@gmail.com>
2026-06-22 11:24:06 +0300

    debug minimal server

--- server/server.js
+++ server/server.js
@@ -1,28 +1,16 @@
 "use strict";
 
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+console.log("STARTING...");
 
+const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "..", "..")));
-
-console.log("FRONTEND DIR:", frontendDir);
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static(frontendDir));
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(frontendDir, "index.html"));
-});
-
-app.get("/health", (req, res) => {
-    res.send("OK");
+    res.send("OK WORKS");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("Server running on " + PORT);
+    console.log("LISTENING ON " + PORT);
 });
